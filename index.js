f956d23988adf8b4350febb27deadced499b9818
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-01-02 10:14:41 +0530

    feat: connect to mongoDB using mongoose

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const fastify = require('fastify');
+const mongoose = require('mongoose');
+
 const app = fastify();
+const mongoURI = process.env.MONGO_URI ||
+  "mongodb+srv://root:<password>@cluster0.znemd.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+try {
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    dbName: 'dev',
+  });
+} catch(err) {
+  console.error(error);
+}
 
 app.get('/', function (request, reply) {
   reply.send("Hello World");
