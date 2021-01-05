1fef80f716359b2ea1e2646f62e433e1a87d82b4
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-01-05 17:46:56 +0530

    test: update registration successful test to check new user in db

--- __tests__/register.test.js
+++ __tests__/register.test.js
@@ -1,9 +1,15 @@
 const request = require("supertest");
 const returnFastify = require("./setup");
+const bcrypt = require('bcryptjs');
+
+const UserCredentials = require('../models/UserCredentials');
 
 describe("New User Registration", () => {
   test("New Users with Unique Username and Valid Password Registered Succesfully", async () => {
+    /* initialize the fastify object */
     const fastify = returnFastify();
+
+    /* make a request */
     const response = await request(fastify.server)
       .post("/register")
       .send({
@@ -11,7 +17,34 @@ describe("New User Registration", () => {
           "password": "P1",
       });
     
+    /* verify request is successful */
     expect(response.body.msg).toEqual("SUCCESS");
     expect(response.status).toEqual(200);
+
+    /* verify new user has been added to database */
+    try {
+      const user = await UserCredentials.findOne({username: "P1"});
+      expect(user.username).toBe("P1");
+
+      let isMatch;
+      try {
+        isMatch = await bcrypt.compare("P1", user.password)
+        expect(isMatch).toBeTruthy();
+      } catch(err) {
+        console.log(err);
+        throw new Error('Error decrypting password');
+      }
+    } catch(err) {
+      console.log(err);
+      throw new Error('Error finding UserCredentials in DB');
+    }
+
+    /* Delete the user added */
+    try {
+      await UserCredentials.findOneAndDelete({username: "P1"});
+    } catch(err) {
+      console.log(err);
+      throw new Error('Error deleting user from db');
+    }
   });
 });
